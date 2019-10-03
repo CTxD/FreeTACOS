d4947a1c03d19d2d589f27b547c1b185404a4ac7
Peter <pmadse13@student.aau.dk>
2019-10-03 15:15:26 +0200

    creates and write stuff

--- ScheludeGenerator/generate_schedule.scala
+++ ScheludeGenerator/generate_schedule.scala
@@ -1,6 +1,7 @@
 import scala.io.Source;
 import scala.xml._;
 import java.io.FileNotFoundException;
+import java.io.{File, PrintWriter}
 
 object ScheduleGenerator{
   def main(args: Array[String]): Unit = {
@@ -8,10 +9,10 @@ object ScheduleGenerator{
 
     try{
       val lines = this.xmlLinesToList(configName);
-
-      for(line <- lines){
-        println(line.toString());
-      }
+      println(generate(lines))
+      val writer = new PrintWriter(new File("gen.cpp" ))
+      writer.write("Hello Scala")
+      writer.close()
     } catch {
       case err : NoClassDefFoundError => {
         println("Class Not Found Exception");
@@ -19,7 +20,13 @@ object ScheduleGenerator{
       case err : FileNotFoundException => {
         println("Input file not found");
       }
+
     }
+  }
+
+  def generate (lines: List[Any]): String = lines match {
+    case Nil => ""
+    case x::xs => x + generate(xs)
   }
 
   def xmlLinesToList(filename : String) : List[Node] = {
