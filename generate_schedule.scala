b48571a4faba34e8e81f5fa7f00fe7331df792e0
Casper Thomsen <casper.thomsen100@gmail.com>
2019-10-03 12:55:10 +0200

    Simple example - getting started

--- generate_schedule.scala
+++ generate_schedule.scala
@@ -1,5 +1,18 @@
+import scala.io.Source;
+import scala.xml._;
+
 object ScheduleGenerator{
   def main(args: Array[String]): Unit = {
-    print("asd")
+    val configName : String = "configuration.xml";
+
+    var lines = this.xmlLinesToList(configName);
+
+    for(line <- lines){
+      println(line);
+    }
+  }
+
+  def xmlLinesToList(filename : String) : List[Node] = {
+    XML.loadFile(filename).toList;
   }
 }
