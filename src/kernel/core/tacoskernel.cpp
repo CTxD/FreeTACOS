2944970c0e0290c84f7697596d1ef25fe512dcff
Peter <pmadse13@student.aau.dk>
2020-01-24 10:06:28 +0100

    wont load

--- src/kernel/core/tacoskernel.cpp
+++ src/kernel/core/tacoskernel.cpp
@@ -1,4 +1,5 @@
 #include "tacoskernel.h"
+#include "../arinc_module.cpp"
 #include <arinc_module.hpp>
 #include <errcode.h>
 #include <fibpart.h>
@@ -19,10 +20,10 @@ CStdlibApp::TShutdownMode CTacosKernel::Run(void)
     mEvent.Clear();
     mTimer.StartKernelTimer(60 * HZ, TimerHandler, this);
     while (1) {
+        CLogger::Get()->Write("FreeTACOS", LogNotice, "In busy loop");
         mEvent.Wait();
     }
     return ShutdownHalt;
-}
 }
 
 void CTacosKernel::TimerHandler(TKernelTimerHandle hTimer, void* pParam, void* pContext)
