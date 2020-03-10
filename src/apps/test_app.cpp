0943b6aa10ea02960f859dcbd68e263778e11e26
Casper <casper.thomsen100@gmail.com>
2020-03-10 14:12:00 +0100

    Test_app commit

--- src/apps/test_app.cpp
+++ src/apps/test_app.cpp
@@ -1,37 +1,61 @@
 #include <apex_kernel.hpp>
 #include <test_app.h>
 
-TestApp::TestApp(CLogger* logger)
-    : Task({10, 10, this, 0x8000, 10, DEADLINE_TYPE::HARD, {"Test Application {MASTER}"}})
+TestApp::TestApp(CLogger* logger) : Task(10, 10, 10, HARD, {"MASTER"})
 {
     this->logger = logger;
 }
 
 void TestApp::Run(void)
 {
-    auto* regs = GetRegs();
+    auto processName = getProcessName().x;
 
-    logger->Write(*getIdentifier().x, LogNotice, "Register x30: %d", regs->x30);
+    logger->Write(*processName, LogNotice, "TESTING APEX");
+    PROCESS_ID_TYPE myId;
+    RETURN_CODE_TYPE idReturn;
 
-    logger->Write(*getIdentifier().x, LogNotice, "Spawning new Process...");
-    RETURN_CODE_TYPE* returnCode;
-    PROCESS_ID_TYPE* id;
+    GET_MY_ID(&myId, &idReturn);
 
-    PROCESS_ATTRIBUTE_TYPE* newProcess = new PROCESS_ATTRIBUTE_TYPE{
-        10, 10, new TestApp(logger), 0x8000, 10, DEADLINE_TYPE::HARD, {"Test Application {SLAVE}"}};
+    PROCESS_STATUS_TYPE myStatus;
+    RETURN_CODE_TYPE statusReturn;
 
-    CREATE_PROCESS(newProcess, id, returnCode);
-    ApexKernel::addToProcessList(static_cast<Task*>(newProcess->ENTRY_POINT));
+    GET_PROCESS_STATUS(myId, &myStatus, &statusReturn);
 
-    if (*returnCode == RETURN_CODE_TYPE::NO_ERROR) {
-        logger->Write(*getIdentifier().x, LogNotice,
-                      "Process Spawned with RC: %i and id: %i", returnCode, id);
+    if (statusReturn != NO_ERROR) {
+        logger->Write("APEX", LogPanic,
+                      "PROCESS_STATUS: Something went wrong! ReturnCode is %i", statusReturn);
     }
     else {
-        logger->Write(*getIdentifier().x, LogNotice, "Error creating process");
+        logger->Write("APEX", LogNotice, "Process Name: %s",
+                      *myStatus.ATTRIBUTES.NAME.x);
     }
 
-    logger->Write(*getIdentifier().x, LogNotice, "ID TEST: %i", id);
+    if (idReturn != NO_ERROR) {
+        logger->Write("APEX", LogPanic,
+                      "Process ID: Something went wrong! ReturnCode is %i", idReturn);
+    }
+    else {
+        logger->Write("APEX", LogNotice, "Process ID: %i -- TaskID is %i", myId,
+                      static_cast<Task*>(myStatus.ATTRIBUTES.ENTRY_POINT)->getId());
+    }
+
+    logger->Write("APEX", LogNotice, "Spawning new Process...");
+    RETURN_CODE_TYPE returnCode;
+    PROCESS_ID_TYPE id;
+
+    PROCESS_ATTRIBUTE_TYPE newProcess = PROCESS_ATTRIBUTE_TYPE{
+        10, 10, new TestApp(logger), 0x8000, 10, HARD, {"SLAVE"}};
+
+    CREATE_PROCESS(&newProcess, &id, &returnCode);
+
+    if (returnCode == RETURN_CODE_TYPE::NO_ERROR) {
+        logger->Write(*processName, LogNotice,
+                      "Process %s Spawned with RC: %i and id: %i",
+                      *newProcess.NAME.x, returnCode, id);
+    }
+    else {
+        logger->Write(*processName, LogPanic, "Error creating process");
+    }
 }
 
 TestApp::~TestApp(void)
