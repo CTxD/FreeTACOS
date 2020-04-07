e4b108d2e5934531e9e2b90806e4ac2c33fa997c
TeiturMcSwagger <Teiturargeseloy@hotmail.com>
2020-04-07 15:18:34 +0200

    Changed Time to a class

--- src/libuser/apex/include/apex_time.hpp
+++ src/libuser/apex/include/apex_time.hpp
@@ -9,19 +9,22 @@
 
 #include "apex_types.hpp"
 
-extern void TIMED_WAIT(
-    /*in */ SYSTEM_TIME_TYPE DELAY_TIME,
-    /*out*/ RETURN_CODE_TYPE* RETURN_CODE);
+class Time {
+public:
+    static void TIMED_WAIT(
+        /*in */ SYSTEM_TIME_TYPE DELAY_TIME,
+        /*out*/ RETURN_CODE_TYPE* RETURN_CODE);
 
-extern void PERIODIC_WAIT(
-    /*out*/ RETURN_CODE_TYPE* RETURN_CODE);
+    static void PERIODIC_WAIT(
+        /*out*/ RETURN_CODE_TYPE* RETURN_CODE);
 
-extern void GET_TIME(
-    /*out*/ SYSTEM_TIME_TYPE* SYSTEM_TIME,
-    /*out*/ RETURN_CODE_TYPE* RETURN_CODE);
+    static void GET_TIME(
+        /*out*/ SYSTEM_TIME_TYPE* SYSTEM_TIME,
+        /*out*/ RETURN_CODE_TYPE* RETURN_CODE);
 
-extern void REPLENISH(
-    /*in */ SYSTEM_TIME_TYPE BUDGET_TIME,
-    /*out*/ RETURN_CODE_TYPE* RETURN_CODE);
+    static void REPLENISH(
+        /*in */ SYSTEM_TIME_TYPE BUDGET_TIME,
+        /*out*/ RETURN_CODE_TYPE* RETURN_CODE);
+};
 
 #endif
