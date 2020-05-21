8475b71674ca4b558aa5b838fcbb4e402d77fa4a
TeiturMcSwagger <Teiturargeseloy@hotmail.com>
2020-05-21 14:42:01 +0200

    Test for create mutex output

--- src/libuser/apex/tests/src/tacoskerneltest.cpp
+++ src/libuser/apex/tests/src/tacoskerneltest.cpp
@@ -1,5 +1,6 @@
 #include <tacoskerneltest.hpp>
 #include <apex_buffer_tests.hpp>
+#include <apex_mutex_tests.hpp>
 
 #include <arch.h>
 
@@ -8,6 +9,7 @@ void setup(){
     CLogger::Get()->Write("APEX TEST", LogNotice,
                               "---------- Starting apex tests ----------");
     TestCreateBufferOutput();
+    TestCreateMutexOutput();
     
     CLogger::Get()->Write("APEX TEST", LogNotice,
                               "-------- All apex tests are done --------");
